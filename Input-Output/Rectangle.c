1ceae5964a24105125f1a04114836b1b1d44f3d6
Prince-Mann94 <princemann2007@gmail.com>
2025-09-30 21:13:00 +0530

    Update Rectangle.c

--- Input-Output/Rectangle.c
+++ Input-Output/Rectangle.c
@@ -1,4 +1,5 @@
 //Write a program to calculate perimeter of rectangle.
+//Author : Prince Mann
 
 #include<stdio.h>
 int main(){
@@ -9,6 +10,6 @@ int main(){
     printf("Enter breadth :");
     scanf("%f",&breadth);
 
-    printf("Perimeter of rectangle :%.2f",2*(length+breadth));
+    printf("Perimeter of rectangle :%.2f",2*(length+breadth));  //%.2f is used for taking upto 2 decimal values
     return 0;
 }
