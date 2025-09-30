e37db4bc78ac573f7bb3f3aea60fd2330daaf9b7
Prince-Mann94 <princemann2007@gmail.com>
2025-09-30 21:20:07 +0530

    Create Circle.c

---
+++ Input-Output/Circle.c
@@ -0,0 +1,12 @@
+//Write a program to calculate perimeter of rectangle.
+//Author : Prince Mann
+
+#include<stdio.h>
+int main(){
+    float radius;
+    printf("Enter radius : ");
+    scanf("%f",&radius);
+
+    printf("Circumference of circle : %.2f",2*3.14*radius);
+    return 0;
+}
