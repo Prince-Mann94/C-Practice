2a67197651f7185a88aba086d24cc62b1c7e1bcb
Prince-Mann94 <princemann2007@gmail.com>
2025-10-06 18:24:55 +0530

    Add files via upload

---
+++ 003 Conditional/collinear.c
@@ -0,0 +1,24 @@
+// WAP to check if three points (x1,y1), (x2,y2) and (x3,y3) are collinear or not.
+#include<stdio.h>
+int main(){
+    int x1,y1;
+    int x2,y2;
+    int x3,y3;
+    int slope1;
+    int slope2;
+
+    printf("Enter first points(x1,y1) : ");
+    scanf("%d %d",&x1,&y1);
+
+    printf("Enter second points(x2,y2) : ");
+    scanf("%d %d",&x2,&y2);
+
+    printf("Enter third points(x3,y3) : ");
+    scanf("%d %d",&x3,&y3);
+
+    if((x2 - x1)*(y3 - y1) == (y2 - y1)*(x3 - x1)){
+        printf("collinear");
+    }else{
+        printf("not colliear");
+    }
+}
